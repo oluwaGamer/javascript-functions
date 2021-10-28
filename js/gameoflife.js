d2fa0bc29cc5fcbcf832fd6bdd930c4e3e71b3ef
oluwaGamer <bakare1627@gmail.com>
2021-10-28 09:57:51 +0100

    Update gameoflife.js

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -23,23 +23,23 @@ const printCell = (cell, state) => {
   return contains.call(state, cell) ? "\u25A3": "u25A2";
 };
 
-const corners = (state = []) => {
-  if (state.length === 0) {
-    return {
-      topRight: [0, 0],
-      bottomLeft: [0, 0]
-    }
-  }
+// const corners = (state = []) => {
+//   if (state.length === 0) {
+//     return {
+//       topRight: [0, 0],
+//       bottomLeft: [0, 0]
+//     }
+//   }
 
-  const xs = state.map(([x, _]) => x);
-  const ys = state.map(([_, y]) => y);
-  return {
-    topRight: [Math.max(...xs), Math.max(...ys)],
-    bottomLeft: [Math.min(...xs), Math.min(...ys)]
-  }
-};
+//   const xs = state.map(([x, _]) => x);
+//   const ys = state.map(([_, y]) => y);
+//   return {
+//     topRight: [Math.max(...xs), Math.max(...ys)],
+//     bottomLeft: [Math.min(...xs), Math.min(...ys)]
+//   }
+// };
 
-const printCells = (state) => {};
+function printCells(state) { }
 
 const getNeighborsOf = ([x, y]) => {};
 
